d42558563977280a4919d962504287276c4b62bc
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-02-19 19:44:36 +0530

    getter created for all the fields

--- src/bylinkedlist/Person.java
+++ src/bylinkedlist/Person.java
@@ -8,5 +8,19 @@ public class Person {
     private ArrayList<Long> contactNumber;
     private String mailID;
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public ArrayList<Long> getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getMailID() {
+        return mailID;
+    }
 }
