1b09673b80b22b532141e39247b827548cbd7ce0
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-02-19 20:27:30 +0530

    method created and completed : getNode

--- src/bylinkedlist/Definition.java
+++ src/bylinkedlist/Definition.java
@@ -5,6 +5,14 @@ public class Definition<E> implements ADT<E> {
     private Node head = null;
     private int size = 0;
 
+    private Node getNode(int index) {
+        Node response = head;
+        for (int i = 0; i < index; i++) {
+            response = response.getNext();
+        }
+        return response;
+    }
+
     @Override
     public void add(Person data) {
 
