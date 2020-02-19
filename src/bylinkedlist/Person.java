538c07a6d4c48f2bc1bde1dd042e88d591bb9a76
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-02-19 19:42:13 +0530

    field created : firstName, lastName, contactNumber, mailID

--- src/bylinkedlist/Person.java
+++ src/bylinkedlist/Person.java
@@ -1,5 +1,12 @@
 package bylinkedlist;
 
+import java.util.ArrayList;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Long> contactNumber;
+    private String mailID;
+
 
 }
