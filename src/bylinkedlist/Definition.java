1d1eab7824409925c4cbef7bfce43b9cfe18f157
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-02-19 20:25:26 +0530

    field created : head, size

--- src/bylinkedlist/Definition.java
+++ src/bylinkedlist/Definition.java
@@ -1,4 +1,27 @@
 package bylinkedlist;
 
-public class Definition {
+public class Definition<E> implements ADT<E> {
+
+    private Node head = null;
+    private int size = 0;
+
+    @Override
+    public void add(Person data) {
+
+    }
+
+    @Override
+    public void remove(Person data) {
+
+    }
+
+    @Override
+    public Person search(String firstName) {
+        return null;
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
